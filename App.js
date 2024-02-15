d4501f4f56cd8aa8cfbd2b609defafdfdd559f0c
Renan Cavichi <renancavichi@gmail.com>
2024-02-15 14:29:38 -0300

    add text style sample

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.boasvindas}>Olá Mundo React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boasvindas: {
+    color: '#568975',
+    backgroundColor: '#DDDDDD',
+    fontSize: 30
+  }
 });
