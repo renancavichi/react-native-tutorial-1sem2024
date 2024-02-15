f1bc96e5978c55957b43eebc35b1a70e6d97ce9d
Renan Cavichi <renancavichi@gmail.com>
2024-02-15 17:01:35 -0300

    sample layout

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.boasvindas}>Olá Mundo React Native!</Text>
+      <View style={styles.header}>
+        <View style={styles.avatar}></View>
+        <Text style={styles.boasvindas}>Renan Cavichi</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boasvindas}>Bem vindo!</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.boasvindas}>Copyright 2024</Text>
+      </View> 
       <StatusBar style="auto" />
     </View>
   );
@@ -18,8 +27,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   boasvindas: {
-    color: '#568975',
-    backgroundColor: '#DDDDDD',
-    fontSize: 30
+    color: '#FFF',
+    fontSize: 25,
+  },
+  header:{
+    flex: 1,
+    backgroundColor: '#564655',
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  body: {
+    flex: 3,
+    backgroundColor: '#765765',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: '#985678',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: '50%',
+    marginHorizontal: 10
   }
-});
+})
