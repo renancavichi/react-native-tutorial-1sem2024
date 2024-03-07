3041b7cf4d4d9e68d2cc52ba9b7d05b700112293
Renan Cavichi <renancavichi@gmail.com>
2024-03-07 16:12:12 -0300

    add FlatList

--- components/Body.js
+++ components/Body.js
@@ -1,25 +1,36 @@
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, FlatList} from 'react-native'
 import H1 from './ui/H1'
 import CardUser from './CardUser'
+
+const users = [
+  {
+    id: 1,
+    name: "Renan Cavichi",
+    email: "renancavichi@gmail.com",
+    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
+  },
+  {
+    id: 2,
+    name: "Maria",
+    email: "maria@gmail.com",
+    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
+  },
+  {
+    id: 3,
+    name: "Manuel",
+    email: "manuel@gmail.com",
+    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
+  }
+]
 
 const Body = () => {
   return (
     <View style={styles.body}>
         <H1 style={styles.usuariosH1}>Usuários</H1>
-        <CardUser
-          avatar="https://avatars.githubusercontent.com/u/4259630?v=4"
-          name="Renan Cavichi"
-          email="renancavichi@gmail.com"
-        />
-        <CardUser
-          avatar="https://avatars.githubusercontent.com/u/114303361?v=4"
-          name="Maria"
-          email="maria@gmail.com"
-        />
-        <CardUser
-          avatar="https://avatars.githubusercontent.com/u/4061891?v=4"
-          name="Manuel"
-          email="manuel@gmail.com"
+        <FlatList
+          data={users}
+          renderItem={({item}) => <CardUser user={item} />}
+          keyExtractor={item => item.id}
         />
     </View>
   )
--- components/CardUser.js
+++ components/CardUser.js
@@ -2,18 +2,18 @@ import {Text, View, StyleSheet} from 'react-native'
 import { Image } from 'expo-image'
 import H4 from './ui/H4'
 
-const CardUser = ({avatar, name, email}) => {
+const CardUser = ({user}) => {
   return (
     <View style={styles.card}>
         <View style={styles.avatar}>
             <Image
                 style={styles.avatarImg}
-                source={avatar}
+                source={user.avatar}
             />
         </View>
         <View>
-            <H4>{name}</H4>
-            <Text style={styles.email}>{email}</Text>
+            <H4>{user.name}</H4>
+            <Text style={styles.email}>{user.email}</Text>
         </View>
     </View>
   )
