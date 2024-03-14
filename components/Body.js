3413d2849a0ea9b037bd315b7b8b57712ab92caf
Renan Cavichi <renancavichi@gmail.com>
2024-03-14 16:50:59 -0300

    get user from api

--- components/Body.js
+++ components/Body.js
@@ -1,56 +1,18 @@
 import {View, StyleSheet, FlatList} from 'react-native'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import H1 from './ui/H1'
 import CardUser from './CardUser'
 
-const users = [
-  {
-    id: 1,
-    name: "Renan Cavichi",
-    email: "renancavichi@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
-  },
-  {
-    id: 2,
-    name: "Maria",
-    email: "maria@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
-  },
-  {
-    id: 3,
-    name: "Manuel",
-    email: "manuel@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
-  },
-  {
-    id: 4,
-    name: "Camila",
-    email: "camila@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
-  },
-  {
-    id: 5,
-    name: "Renan Cavichi",
-    email: "renancavichi@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
-  },
-  {
-    id: 6,
-    name: "Maria",
-    email: "maria@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
-  },
-]
-
-
-
 const Body = () => {
 
-  const getUsers = async () =>{
+  const [users, setUsers] = useState([])
+
+  const getUsers = async () => {
     try{
-      const result = await fetch('https://brasilapi.com.br/api/cvm/corretoras/v1')
+      const result = await fetch('https://backend-api-express-1sem2024-rbd1.onrender.com/user')
       const data = await result.json()
-      console.log(data[0].nome_social)
+      console.log(data.success)
+      setUsers(data.users)
     } catch (error){
       console.log(error.message)
     }
