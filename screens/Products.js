9ac2f40151170a9ac3473eeabebe2f8eb3ffed8f
Renan Cavichi <renancavichi@gmail.com>
2024-04-25 15:36:22 -0300

    sample AsyncStorage

--- screens/Products.js
+++ screens/Products.js
@@ -1,16 +1,53 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TextInput} from 'react-native'
 import Button from '../components/ui/Button.js'
-import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
 const Products = () => {
+  const [nameUser, setNameUser] = useState('')
+  
+  const saveAS = async (data, value) => {
+    try {
+      await AsyncStorage.setItem(data, value)
+    } catch (error){
+      console.log('Erro ao gravar dado')
+    }
+  }
 
-    const navigation = useNavigation()
+  const getAS = async (data) => {
+    let dataFound = null
+    try {
+      dataFound = await AsyncStorage.getItem(data)
+    } catch (error){
+      console.log('Erro ao ler dado')
+    }
+    return dataFound
+  }
+
+  useEffect(() => {
+    const getNome = async () =>{
+      const nome = await getAS('nome')
+      setNameUser(nome)
+    }
+
+    getNome()
+  },[])
 
   return (
     <View style={styles.container}>
-      <Text>Products</Text>
-        <Button title="Cadastrar User" 
-        onPress={() => navigation.navigate('Cadastrar')}
+      
+      <Text>Nome salvo: {nameUser}</Text>
+
+      <Text>Exemplo AsyncStorage</Text>
+
+      <TextInput 
+                style={styles.input}
+                placeholder='Nome...'
+                onChangeText={setNameUser}
+                value={nameUser}
+                /> 
+        <Button title="Cadastrar Nome User" 
+        onPress={() => saveAS('nome', nameUser)}
         />
     </View>
   )
@@ -21,7 +58,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    input: {
+      height: 40,
+      width: '100%',
+      backgroundColor: '#FFF',
+      borderWidth: 1,
+      marginBottom: 18,
+      padding: 10,
+  }
 })
 
 export default Products
